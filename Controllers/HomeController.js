449ec1e74466dcf3892c094f74fdfb7b558efab2
chriseadie <caeadie@hotmail.com>
2019-07-09 22:52:21 +0100

    added styles sheet and set up layout template for project

--- Controllers/HomeController.js
+++ Controllers/HomeController.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const app = express();
 const router = express.Router();
+app.use(express.static("Assets"));
 
 router.get("/", (req, res) => {
   res.render("./Homepage/index.njk");
