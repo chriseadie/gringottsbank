743b71f478a7d5a027b629875906815232a59651
chriseadie <caeadie@hotmail.com>
2019-07-11 22:00:03 +0100

    Added routes in home controller for Daily Profit and Login, Created pages for login and blogs and started styling, Created dummy data for Daily profit page

--- Controllers/HomeController.js
+++ Controllers/HomeController.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const router = express.Router();
@@ -5,6 +6,19 @@ app.use(express.static("Assets"));
 
 router.get("/", (req, res) => {
   res.render("./Homepage/index.njk");
+});
+
+router.get("/login", (req, res) => {
+  res.render("./Homepage/login.njk");
+});
+
+router.get("/dailyprofit", (req, res) => {
+  fs.readFile("./LocalData/BlogPostsMock.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("./Homepage/blog.njk", JSON.parse(data));
+  });
 });
 
 module.exports = router;
